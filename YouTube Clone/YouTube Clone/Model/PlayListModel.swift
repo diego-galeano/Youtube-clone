5871c3302694aa9972584ff09ccef250b42534f1
Diego-Galeano <diegogaleano.ar@gmail.com>
2023-01-03 20:31:38 -0300

    first commit, starting the proyect

---
+++ YouTube Clone/YouTube Clone/Model/PlayListModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlayListModel.swift
+//  YouTube Clone
+//
+//  Created by Diego Galeano on 03/01/2023.
+//
+
+import Foundation
---
+++ YouTube Clone/YouTube Clone/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  YouTube Clone
+//
+//  Created by Diego Galeano on 03/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabelUI: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myLabelUI.text = "HOLA MUNDO"
+        
+       
+        
+    }
+
+
+}
+
